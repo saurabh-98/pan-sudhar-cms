b91fcbae86f17fb13b7e4c2f72d6c2fa11f10c13
saurabh-98 <saurabhkumarjha323@gmail.com>
2026-07-06 14:31:06 +0530

    update migartion pan hfin h

--- database/migrations/2026_07_06_141945_update_pan_find_histories_table.php
+++ database/migrations/2026_07_06_141945_update_pan_find_histories_table.php
@@ -11,7 +11,6 @@ return new class extends Migration
         Schema::table('pan_find_histories', function (Blueprint $table) {
 
             $table->string('application_no')
-                ->unique()
                 ->after('id');
 
             $table->string('payment_status')
