ae29a0f783caf0f08a297519f953148b06ada57a
saurabh-98 <saurabhkumarjha323@gmail.com>
2026-07-08 14:42:37 +0530

    updated

--- resources/views/auth/retailer/register.blade.php
+++ resources/views/auth/retailer/register.blade.php
@@ -578,13 +578,7 @@
 @endsection
 
 
-@section('scripts')
 
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
-<script src="https://www.google.com/recaptcha/api.js"
-        async
-        defer></script>
 
 @section('scripts')
 
