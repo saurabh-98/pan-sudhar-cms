7baaf6704d0041613b30bf695827a89d898c1642
saurabh-98 <saurabhkumarjha323@gmail.com>
2026-07-10 14:39:54 +0530

    update

--- app/Helpers/BankAccountFormHelper.php
+++ app/Helpers/BankAccountFormHelper.php
@@ -70,79 +70,87 @@ if (! function_exists('bank_account_fields')) {
         | differs for it.
         */
 
+           $customerName = [
+            'name'     => 'customer_name',
+            'label'    => 'Customer Name',
+            'type'     => 'text',
+            'required' => true,
+        ];
+
+        $customerMobile = [
+            'name'     => 'mobile',
+            'label'    => 'Mobile Number',
+            'type'     => 'text',
+            'required' => true,
+        ];
+
+        $aadhaarNumber = [
+            'name'     => 'aadhaar_number',
+            'label'    => 'Aadhaar Number',
+            'type'     => 'text',
+            'required' => false,
+        ];
+
+        $passportPhotoField = [
+            'name'        => 'passport_size_photo',
+            'label'       => 'Passport Size Photo Available?',
+            'type'        => 'select',
+            'placeholder' => 'Select',
+            'options'     => [
+                'yes' => 'Yes',
+                'no'  => 'No',
+            ],
+            'required'    => false,
+        ];
+
         $emailField = [
-            'name' => 'email',
-            'label' => 'Email Address',
-            'type' => 'email',
+            'name'     => 'email',
+            'label'    => 'Email Address',
+            'type'     => 'email',
+            'required' => false,
         ];
 
         $addressField = [
-            'name' => 'address',
-            'label' => 'Address',
-            'type' => 'textarea',
+            'name'     => 'address',
+            'label'    => 'Address',
+            'type'     => 'textarea',
             'required' => true,
         ];
 
         $panNumberField = [
-            'name' => 'pan_number',
-            'label' => 'PAN Number',
-            'type' => 'text',
-            'required' => true,
+            'name'     => 'pan_number',
+            'label'    => 'PAN Number',
+            'type'     => 'text',
+            'required' => false,
         ];
 
         $panCardField = [
-            'name' => 'pan_card',
-            'label' => 'PAN Card',
-            'type' => 'file',
+            'name'     => 'pan_card',
+            'label'    => 'PAN Card',
+            'type'     => 'file',
             'required' => true,
         ];
 
         $initialDepositField = [
-            'name' => 'initial_deposit_amount',
-            'label' => 'Initial Deposit Amount',
-            'type' => 'number',
+            'name'     => 'initial_deposit_amount',
+            'label'    => 'Initial Deposit Amount',
+            'type'     => 'number',
             'required' => false,
         ];
 
         $nomineeFields = [
-
             [
-                'name' => 'nominee_name',
-                'label' => 'Nominee Name',
-                'type' => 'text',
+                'name'     => 'nominee_name',
+                'label'    => 'Nominee Name',
+                'type'     => 'text',
                 'required' => true,
             ],
-
             [
-                'name' => 'nominee_relation',
-                'label' => 'Nominee Relation',
-                'type' => 'text',
+                'name'     => 'nominee_relation',
+                'label'    => 'Nominee Relation',
+                'type'     => 'text',
                 'required' => true,
             ],
-
-        ];
-
-        $occupationField = [
-            'name' => 'occupation',
-            'label' => 'Occupation',
-            'type' => 'select',
-            'options' => [
-                'Salaried',
-                'Self Employed',
-                'Business',
-                'Student',
-                'Homemaker',
-                'Retired',
-                'Other',
-            ],
-            'required' => true,
-        ];
-
-        $preferredBranchField = [
-            'name' => 'preferred_branch',
-            'label' => 'Preferred Branch / City',
-            'type' => 'text',
-            'required' => true,
         ];
 
         return [
@@ -178,18 +186,14 @@ if (! function_exists('bank_account_fields')) {
             | nominee are all mandatory.
             */
 
-            'indian-bank' => array_merge(
-                $baseFields,
-                [
-                    $panNumberField,
-                    $panCardField,
-                    $emailField,
-                    $addressField,
-                    $preferredBranchField,
-                    $initialDepositField,
-                ],
-                $nomineeFields
-            ),
+            'indian-bank' => array_merge([
+                $customerName,
+                $customerMobile,
+                $aadhaarNumber,
+                $passportPhotoField, 
+                $panNumberField,
+                
+            ]),
 
             /*
             |--------------------------------------------------------------------------
